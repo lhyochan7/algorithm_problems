c6323f1f3ca2d7186d320383d8bc0e06b665815b
hyochan <lhyochan7@gmail.com>
2022-01-22 18:44:14 +0900

    updated on 2022/01/22

--- Level 2/124나라의숫자.py
+++ Level 2/124나라의숫자.py
@@ -1,14 +1,18 @@
+# 124 나라의 숫자
 def solution(n):
     answer = ''
     
-    dict124 = {1:1, 2:2, 3:4, 4:11, 5:12, 6:14, 7:21, 8:22, 9:24, 10:41}
+    #우선 n이 3의 배수가 아니라면 3진법을 구하는 것과 동일하게 3으로 나눈 나머지를 저장하고, n을 3으로 나눈 몫으로 저장한다.
+    #n이 3의 배수라면 무조건 4를 추가하고, n을 3으로 나눈 몫에서 1을 뺀 값을 저장한다.
+    while n:
+        # 3의 배수가 아닐떄
+        if n % 3:
+            answer += str(n % 3)
+            n //= 3
+        # 3의 배수
+        else:
+            answer += "4"
+            n = n//3 - 1
     
-    n = str(n)
-    temp = list()
-    
-    for num in n:
-        temp.append(dict124[int(num)])
-    
-    answer = "".join(map(str,temp))
-    
-    return answer
+    #역순으로 반환
+    return answer[::-1]
