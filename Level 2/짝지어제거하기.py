4f9d3e5ef4123fcea9fa9223dac8ea39c11a17f6
hyochan <lhyochan7@gmail.com>
2022-02-05 21:12:24 +0900

    updated on 2022/02/05

--- Level 2/짝지어제거하기.py
+++ Level 2/짝지어제거하기.py
@@ -1,14 +1,28 @@
+# Method using stack
+
 def solution(s):
-    if len(s) % 2 == 1: return 0 # 문자가 홀수개면 무조건 남는다.
-    if len(s) == 2: # 문자가 2개이고, 같은 문자라면 무조건 가능
-        return 1 if s[0] == s[1] else 0
-            
-    stack = [s[0]]
+    answer = -1
     
-    for v in s[1:]:
-        if len(stack) > 0 and stack[-1] == v:
-            stack.pop()
+    stck = []
+
+    for i in s:
+        # if stack is empty
+        if len(stck) == 0:
+            stck.append(i)
+            continue
+
+        b = stck[-1]
+        
+        # remove from stack if pair
+        if i == b:
+            stck.pop()
+        # add onto stack if not pair
         else:
-            stack.append(v)
+            stck.append(i)
             
-    return 0 if len(stack) else 1
+    if len(stck) == 0:
+        answer = 1
+    else:
+        answer = 0
+    
+    return answer
