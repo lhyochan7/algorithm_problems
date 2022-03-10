5c3815e00fa2435466a583db994ee875d591042a
hyochan <lhyochan7@gmail.com>
2022-03-10 21:24:36 +0900

    updated on 2022/03/10

--- baekjoon/1260.py
+++ baekjoon/1260.py
@@ -1,15 +1,16 @@
 from collections import deque
 
 def dfs(graph, start):
+    graph = {key : sorted(graph[key], reverse=True) for key in graph}
+    print("Reverse Sorted Graph: ", graph)
+
     visited, need_visit = list(), deque()
     need_visit.append(start)
 
     while need_visit:
-        print("Need visit = ", need_visit)
+       # print("Need visit = ", need_visit)
         node = need_visit.pop()
-
-        print("node = ", node)
-
+        #print("node = ", node)
         if node not in visited:
             visited.append(node)
             need_visit.extend(graph[node])
@@ -38,12 +39,13 @@ if __name__ == '__main__':
         a, b = map(int, input().split())
         graph[a].append(b)
         graph[b].append(a)
+        graph[a].sort()
+        graph[b].sort()
 
-
-    print(graph)
+    print("Sorted Graph:", graph)
 
     dfs_visited = dfs(graph, v)
     bfs_visited = bfs(graph, v)
 
-    print(dfs_visited)
-    print(bfs_visited)
+    print("dfs = ", dfs_visited)
+    print("bfs = ", bfs_visited)
