26e23240f4e9c232a73f77f3b08b6b311a03f048
hyochan <lhyochan7@gmail.com>
2022-03-19 23:21:40 +0900

    updated on 2022/03/19

--- baekjoon/BFS,DFS/2146.py
+++ baekjoon/BFS,DFS/2146.py
@@ -1,5 +1,20 @@
 from collections import deque
 
+def bfs1(graph, start):
+    visited, need_visit = list(), deque()
+
+    need_visit.append(start)
+
+    while need_visit:
+        node = need_visit.popleft()
+
+        if node not in visited:
+            visited.append(node)
+            need_visit.extend(graph[node])
+
+    return visited
+
+
 if __name__ == '__main__':
     n = int(input())
 
@@ -8,6 +23,7 @@ if __name__ == '__main__':
     for i in range(n):
         temp = input().split()
         graph[i].extend(temp)
+    
 
     print(graph)
 
