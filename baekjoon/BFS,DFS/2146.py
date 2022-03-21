c7d1a092b29f2ce84fcc22b67ad9cc7450a6ff63
hyochan <lhyochan7@gmail.com>
2022-03-21 22:58:52 +0900

    updated on 2022/03/21

--- baekjoon/BFS,DFS/2146.py
+++ baekjoon/BFS,DFS/2146.py
@@ -1,34 +1,57 @@
 from collections import deque
 
-def bfs1(graph, start):
+def bfs(graph, start, n):
     visited, need_visit = list(), deque()
 
     need_visit.append(start)
+    
+    for x in range(n):
+        for y in range(n):
+            if y < 0 or y >= n or x < 0 or x >= n:
+                continue
 
-    while need_visit:
-        node = need_visit.popleft()
+            print(graph[i][j], end=' ')
+        print('-------------------------')
 
-        if node not in visited:
-            visited.append(node)
-            need_visit.extend(graph[node])
+
+
+    # while need_visit:
+    #     node = need_visit.popleft()
+
+    #     if node not in visited:
+    #         visited.append(node)
+    #         need_visit.extend(graph[node])
 
     return visited
 
-
+#%%
 if __name__ == '__main__':
     n = int(input())
-
-    graph = [[] for _ in range(n)]
+    graph = [[0] * n for _ in range(n)]
 
     for i in range(n):
-        temp = input().split()
-        graph[i].extend(temp)
-    
+        temp = map(int, input().split())
+        a = []
+        a.extend(temp)
 
+        for j, value in enumerate(a):
+            graph[i][j] = value
+            # idx += 1
+            # num = (i*n) + idx
+            # print("n = %d  (%d, %d) = %d / num = %d" %(n, i, idx, j, num))
+            # graph[num].append(j)
+
+    islands = {}
+
+    for i in range(n):
+        for j in range(n):
+            
+            visited = bfs(graph, node, n)
+        #visited = sorted(visited)
+        #print(visited)
+        #islands.add(visited)
+            
     print(graph)
+    print(islands)
 
-'''
-    for x in range(n):
-        for y in range(n):
-            if 
-'''
+# %%
