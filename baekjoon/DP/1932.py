399b5b93a7311d7086e190a2762397935ab8e519
lhyochan7 <lhyochan7@gmail.com>
2022-05-09 12:13:28 +0900

    added on 2022/05/09

---
+++ baekjoon/DP/1932.py
@@ -0,0 +1,24 @@
+import sys
+
+N = int(input())
+
+graph = [[0] * cnt for cnt in range(1,N+1)]
+
+for i in range(N):
+    input = list(map(int, sys.stdin.readline().split()))
+    graph[i] = input
+
+
+cost_table = [[0] * N for _ in range(1,N+1)]
+
+for i, nodes in enumerate(graph):
+    for j, node in enumerate(nodes):
+        if i == 0:
+            cost_table[i][j] = graph[i][j]            
+        else:
+            cost_table[i][j] = max(cost_table[i-1][j-1] + graph[i][j], cost_table[i-1][j] + graph[i][j])
+
+
+answer = max(cost_table[N-1])
+
+print(answer)
