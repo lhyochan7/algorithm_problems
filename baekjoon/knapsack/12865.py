2a8aa1fbf4f6d849f4c24a59ca8ac4a723d9b295
hyochan <lhyochan7@gmail.com>
2022-05-29 19:54:18 +0900

    solved on 2022/05/29

--- baekjoon/knapsack/12865.py
+++ baekjoon/knapsack/12865.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     items = [[0,0] for _ in range(N+1)]
     knapsack = [[0]*(K+1) for _ in range(N+1)]
 
-    for i in range(N):
+    for i in range(1, N+1):
         w, v = map(int, input().split())
         items[i][0] = w
         items[i][1] = v
