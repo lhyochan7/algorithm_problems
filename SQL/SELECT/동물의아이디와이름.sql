95c7d59bd5727b8a08a1e17a97c1293e2ff289db
hyochan <lhyochan7@gmail.com>
2022-08-07 00:10:13 +0900

    added on 2022/08/07

---
+++ SQL/SELECT/동물의아이디와이름.sql
@@ -0,0 +1,3 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/5940 --
+
+SELECT animal_id, name from animal_ins;
---
+++ SQL/SELECT/여러기준으로정렬하기.sql
@@ -0,0 +1,2 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59404 --
+SELECT animal_id, name, datetime from animal_ins order by name asc, datetime desc;
